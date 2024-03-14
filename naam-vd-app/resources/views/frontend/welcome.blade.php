528e54aed5867c3da742a2987dc0c0b6025dfabf
Burak <6028083@mborijnland.nl>
2024-03-14 11:44:14 +0100

    opdr3

---
+++ naam-vd-app/resources/views/frontend/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('frontend.layout')
+@section('content')
+Welcome
+@endsection
+
+@section('footer')
+Welcome Footer
+@endsection
---
+++ naam-vd-app/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Controller; // Ensure you have a semicolon after Controller
+use App\Http\Controllers\FrontendController;
+
+Route::get('/', [FrontendController::class, 'index']);
+
+Route::get('/contact', [FrontendController::class, 'contact']);
+Route::get('/about', [FrontendController::class, 'about']);
